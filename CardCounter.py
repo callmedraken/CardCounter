037221800b196c9a20569c4a225704609acbb959
draken <jhsiao1996@gmail.com>
2019-07-30 19:15:31 -0700

    Began card counting loop

--- CardCounter.py
+++ CardCounter.py
@@ -1,6 +1,7 @@
 ### John Hsiao 7/30/2019
 ### Card counter program for playing blackjack
 
+import sys
 
 def cardCounting(deckCount):
     switcher = {
@@ -23,13 +24,24 @@ def cardCounting(deckCount):
         "K": -1
     }
     runningCount = 0
-    trueCount = 0
+    remainingDecks = deckCount
+    trueCount = runningCount/remainingDecks
+    while True:
+        inp = input("Dealt card: ")
+        
+        if inp == "quit":
+            sys.exit(0)
+
 
 
 def main():
     print("Welcome to the Card Counter Program")
+    print("Input \"quit\" at any time to exit the program")
     deckCount = input("Input the number of decks being used: ")
+    if deckCount == "quit":
+        sys.exit(0)
     cardCounting(deckCount)
+    print("Thanks for playing!")
 
 if __name__ == '__main__':
     main()
