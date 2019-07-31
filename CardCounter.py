e2897038cfe733c1593ce8f5807e4ab0e58524e3
draken <jhsiao1996@gmail.com>
2019-07-30 19:30:33 -0700

    fixed dictionary

--- CardCounter.py
+++ CardCounter.py
@@ -2,42 +2,51 @@
 ### Card counter program for playing blackjack
 
 import sys
+import math
 
 def cardCounting(deckCount):
     switcher = {
-        1: -1,
+        "1": -1,
         "A": -1,
-        2: 1,
-        3: 1,
-        4: 1,
-        5: 1,
-        6: 1,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: -1,
-        11: -1,
+        "a": -1,
+        "2": 1,
+        "3": 1,
+        "4": 1,
+        "5": 1,
+        "6": 1,
+        "7": 0,
+        "8": 0,
+        "9": 0,
+        "10": -1,
+        "11": -1,
         "J": -1,
-        12: -1,
+        "j": -1,
+        "12": -1,
         "Q": -1,
-        13: -1,
-        "K": -1
+        "q": -1,
+        "13": -1,
+        "K": -1,
+        "k": -1
     }
     runningCount = 0
-    remainingDecks = deckCount
-    trueCount = runningCount/remainingDecks
+    totalCards = deckCount * 52
+
     while True:
-        inp = input("Dealt card: ")
-        
+        inp = str(input("Dealt card: "))
+        card = switcher.get(inp)
         if inp == "quit":
             sys.exit(0)
+        runningCount += card
+        totalCards -= 1
+        remainingDecks = math.floor(totalCards / 52)
+        trueCount = runningCount / remainingDecks
 
 
 
 def main():
     print("Welcome to the Card Counter Program")
     print("Input \"quit\" at any time to exit the program")
-    deckCount = input("Input the number of decks being used: ")
+    deckCount = int(input("Input the number of decks being used: "))
     if deckCount == "quit":
         sys.exit(0)
     cardCounting(deckCount)
